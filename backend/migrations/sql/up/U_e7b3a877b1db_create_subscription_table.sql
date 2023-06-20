280661c307db114107a0206b6deee662d9c082fb
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-06-20 10:43:30 -0500

    wip

--- backend/migrations/sql/up/U_e7b3a877b1db_create_subscription_table.sql
+++ backend/migrations/sql/up/U_e7b3a877b1db_create_subscription_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE public.subscription (
 	id bigserial NOT NULL,
 	webhook_url text NOT NULL,
 	feed_url text NOT NULL,
-    inserted_at timestamp(0) NOT NULL,
+	inserted_at timestamp(0) NOT NULL,
 	CONSTRAINT subscription_pkey PRIMARY KEY (id)
 );
