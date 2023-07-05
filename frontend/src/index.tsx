0cc4dc62770e66271c8a0d383176538fd7fceeab
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-07-05 17:57:20 -0500

    add landing page (#20)
    * add boopstrap
    remove useless files
    
    * wip
    
    * Add intial draft
    
    * fix conflicts
    
    * reset
    
    * fix deps
    
    * Update logos
    
    * Update README.md

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import Landing from "./pages/Landing";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
