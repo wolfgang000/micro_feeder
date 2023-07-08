e41b8e0fee5fe316562c38303f9f378c3a23f481
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-07-07 23:35:34 -0500

    improve tests

--- backend/tests/helper.py
+++ backend/tests/helper.py
@@ -6,21 +6,13 @@ from starlette.responses import JSONResponse
 from uuid import uuid4
 
 
-async def create_user_and_login(request):
-    data = await request.json()
-    email = data["email"]
-    uow = unit_of_work.SqlAlchemyUnitOfWork()
-    user = await create_user(uow, {"email": email})
-    request.session.update({"user_id": user.id})
-    return JSONResponse({"user_id": user.id})
-
-
-app.add_route("/create_user_and_login", create_user_and_login, methods=["POST"])
-
-
 def create_authenticated_client():
     email = f"{uuid4()}@example.com"
     client = TestClient(app)
-    response = client.post("/create_user_and_login", json={"email": email})
-    assert response.status_code == 200
+    response = client.get(
+        "/testing/create_user_and_login/",
+        params={"user_email": email},
+        follow_redirects=False,
+    )
+    assert response.status_code == 307
     return client
