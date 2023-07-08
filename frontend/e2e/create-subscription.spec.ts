3a894c0b9c3796837c62b90006a837311183a2ce
wolfgang000 <wolfgangsalazarmora@gmail.com>
2023-07-07 20:53:04 -0500

    wip

--- frontend/e2e/create-subscription.spec.ts
+++ frontend/e2e/create-subscription.spec.ts
@@ -2,17 +2,19 @@ import { test, expect } from "@playwright/test";
 import { CreateSubscriptionPage } from "./pages-objects/create-subscription-page";
 import { ListSubscriptionsPage } from "./pages-objects/list-subscriptions-page";
 import { createUserAndLogin } from "./util";
+import { v4 } from "uuid";
 
 test("Create subscription with valid data", async ({ page }) => {
   await createUserAndLogin(page);
   const createSubscriptionPage = new CreateSubscriptionPage(page);
   const listSubscriptionsPage = new ListSubscriptionsPage(page);
+  const feedUrl = `http://example-feed.com/${v4()}`;
+  const webhookUrl = `http://example-webhook.com/${v4()}`;
 
   await createSubscriptionPage.goto();
   await createSubscriptionPage.validateCurrentUrl();
-  await createSubscriptionPage.createSubscription(
-    "http://example.com/",
-    "http://example.com/"
-  );
+  await createSubscriptionPage.createSubscription(feedUrl, webhookUrl);
   await listSubscriptionsPage.validateCurrentUrl();
+  await expect(page.locator("body")).toContainText(feedUrl);
+  await expect(page.locator("body")).toContainText(webhookUrl);
 });
--- frontend/src/pages/ListSubscriptions/index.tsx
+++ frontend/src/pages/ListSubscriptions/index.tsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./style.css";
 import { core } from "../../api";
 
 function Page() {
-  return <div className="container"></div>;
+  const [subscriptions, setSubscriptions] = useState([] as any[]);
+
+  useEffect(() => {
+    core.getSubscriptionas().then((subscriptions) => {
+      setSubscriptions(subscriptions);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <ul>
+        {subscriptions.map((subscription, index) => (
+          <li key={index}>
+            {subscription.webhook_url} - {subscription.feed_url}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Page;
